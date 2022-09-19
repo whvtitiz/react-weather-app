0520e5e95480c1158b0fe16e458df681a4ee3269
whvtitiz <whitttwiles@gmail.com>
2022-09-18 17:54:29 -0700

    Added the GitHub Repo Url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Whitney Wiles and is{" "}
+        <a href="https://github.com/whvtitiz/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
