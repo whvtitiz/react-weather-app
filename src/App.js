993f93f919529a2ac042bda7655d0774b7d25914
whvtitiz <whitttwiles@gmail.com>
2022-09-18 18:24:48 -0700

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by Whitney Wiles and is{" "}
-        <a href="https://github.com/whvtitiz/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          This project was coded by Whitney Wiles and is{" "}
+          <a
+            href="https://github.com/whvtitiz/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
